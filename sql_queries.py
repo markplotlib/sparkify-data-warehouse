48928a6b9f84d4c648a5ce3258caf9fbb676e56b
markplotlib <mark.chesney@gmail.com>
2020-10-25 13:40:44 -0700

    create table staging_events

--- sql_queries.py
+++ sql_queries.py
@@ -7,7 +7,7 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
 staging_songs_table_drop = ""
 songplay_table_drop = ""
 user_table_drop = ""
@@ -18,6 +18,9 @@ time_table_drop = ""
 # CREATE TABLES
 
 staging_events_table_create= ("""
+                              CREATE TABLE IF NOT EXISTS staging_events (
+                              userId int NOT NULL
+                              )
 """)
 
 staging_songs_table_create = ("""
@@ -65,7 +68,9 @@ time_table_insert = ("""
 
 # QUERY LISTS
 
-create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+# create_table_queries = [staging_events_table_create, staging_songs_table_create, songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+# drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [staging_events_table_create]
+drop_table_queries = [staging_events_table_drop]
 copy_table_queries = [staging_events_copy, staging_songs_copy]
 insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
